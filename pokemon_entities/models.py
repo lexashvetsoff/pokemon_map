d4fa75299137f11f753d689d4a66c8a8818b6683
lexashvetsoff <lexa.shvetsoff@yandex.ru>
2022-04-09 10:22:54 +0800

    Запрещено каскадное удаление

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -9,7 +9,7 @@ class Pokemon(models.Model):
     previous_evolution = models.ForeignKey(
         'Pokemon',
         verbose_name='Из кого эволюционирует',
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='next_evolutions'
